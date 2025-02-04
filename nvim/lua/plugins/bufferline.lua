49d69e121816240e932e60249a688f9ffab173f0
josephMG <joseph@taxigo.com.tw>
2025-02-04 15:35:57 +0800

    update nvim, add move buffer shortcut

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,15 +1,14 @@
 return {
-  'akinsho/bufferline.nvim',
+  "akinsho/bufferline.nvim",
   version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons',
+  dependencies = "nvim-tree/nvim-web-devicons",
+  options = {},
   diagnostics_indicator = function(count, level, diagnostics_dict, context)
     local s = " "
     for e, n in pairs(diagnostics_dict) do
-      local sym = e == "error" and " "
-      or (e == "warning" and " " or " ")
+      local sym = e == "error" and " " or (e == "warning" and " " or " ")
       s = s .. n .. sym
     end
     return s
-  end
+  end,
 }
-
