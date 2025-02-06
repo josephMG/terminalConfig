c47506b86f0350f6a46f227f6d1cd2f9261d37b2
josephMG <joseph@taxigo.com.tw>
2025-02-07 04:08:15 +0800

    update nvim, add autocmds:

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.options")
 require("config.keymaps")
+require("config.autocmds")
 require("config.lazy")
 require("config.plugins.keymaps")
 require("config.plugins.options")
--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,9 +7,7 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "blue" })
-vim.api.nvim_set_hl(0, "LineNr", { fg = "yellow" })
-vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "magenta" })
+os.execute("aulimit -n 1000000")
 
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "*.py",
