35946e821215a05ee78ec3cecdf0067d6a5c5f34
JosephMG <joseph780116.mg@gmail.com>
2025-03-14 10:27:01 +0800

    update nvim, update color

--- nvim/lua/config/plugins/options.lua
+++ nvim/lua/config/plugins/options.lua
@@ -1,10 +1,11 @@
-function LineNumberColors()
+function CustomizeColor()
   local colors = require("catppuccin.palettes").get_palette()
-  vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.surface1, bold = true })
-  vim.api.nvim_set_hl(0, "LineNr", { fg = colors.pink, bold = true })
-  vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.surface1, bold = true })
+  -- vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.surface1, bold = true })
+  -- vim.api.nvim_set_hl(0, "LineNr", { fg = colors.pink, bold = true })
+  -- vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.surface1, bold = true })
+  vim.api.nvim_set_hl(0, "Search", { bg = colors.maroon, fg = colors.surface1 })
 end
 
 -- catppuccin colorscheme
 vim.cmd.colorscheme("catppuccin-mocha")
--- LineNumberColors()
+CustomizeColor()
