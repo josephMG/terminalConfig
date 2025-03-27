e3f6201a06886565fcbdefd8bedf499bb732f193
JosephMG <joseph780116.mg@gmail.com>
2025-03-27 22:11:30 +0800

    add ai shortcut

--- nvim/lua/plugins/ai-assistant.lua
+++ nvim/lua/plugins/ai-assistant.lua
@@ -134,11 +134,10 @@ return {
             command = true,
             model = {
               model = "deepseek-r1:8b",
-              temperature = 0.6,
-              top_p = 1,
-              min_p = 0.05,
             },
-            system_prompt = "You are a general AI assistant.",
+            system_prompt = "I am an AI meticulously crafted to provide programming guidance and code assistance. "
+              .. "To best serve you as a computer programmer, please provide detailed inquiries and code snippets when necessary, "
+              .. "and expect precise, technical responses tailored to your development needs.\n",
           },
           {
             name = "CodeOllamaLlama3.1-8B", -- standard agent name to disable
@@ -149,8 +148,164 @@ return {
             disable = true,
           },
         },
+        hooks = {
+          -- example of usig enew as a function specifying type for the new buffer
+          CodeReview = function(gp, params)
+            local template = "I have the following code from {{filename}}:\n\n"
+              .. "```{{filetype}}\n{{selection}}\n```\n\n"
+              .. "Please analyze for code smells and suggest improvements."
+            local agent = gp.get_chat_agent()
+            gp.Prompt(params, gp.Target.enew("markdown"), agent, template)
+          end,
+          -- example of making :%GpChatNew a dedicated command which
+          -- opens new chat with the entire current buffer as a context
+          BufferChatNew = function(gp, _)
+            -- call GpChatNew command in range mode on whole buffer
+            vim.api.nvim_command("%" .. gp.config.cmd_prefix .. "ChatNew")
+          end,
+          -- ReactIconSvg = function(gp, params)
+          --   local buf = vim.api.nvim_get_current_buf()
+          --   local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+          --   local content = table.concat(lines, "\n")
+          --   local template = "The following SVG code needs to be converted into a valid React component:\n\n"
+          --     .. "INPUT:\n"
+          --     .. "```tsx\n"
+          --     .. content
+          --     .. "```\n\n"
+          --     .. "  - Remove the `width` and `height` props from the `<svg>` element\n"
+          --     .. "  - Add `{...props}` to the bottom of the `<svg>` element\n"
+          --     .. "  - Replace all `fill` values with `currentColor`\n"
+          --     .. "  - Replace all props that are dash-separated (ex: `fill-rule`) with camelCase (ex: `fillRule`)\n"
+          --     .. "  - Don't remove any other props or attributes\n"
+          --     .. "  - Preserve the indentation rules\n"
+          --     .. "  - Only include the code snippet, no additional context or explanation is needed."
+          --   local agent = gp.get_command_agent()
+          --   gp.logger.info("Updating React SVG: " .. agent.name)
+          --   gp.Prompt(params, gp.Target.rewrite, agent, template, nil)
+          -- end,
+          UiIconExport = function(gp, params)
+            local template = "The following React modules need to be refactored and properly exported:\n\n"
+              .. "```tsx\n{{selection}}\n```\n\n"
+              .. "  - Take the unused import at the bottom of the file and move it up to the other imports in the alphabetical orrder\n"
+              .. "  - Export the unsed import in the `icons` array in alphabetical order\n"
+              .. "  - Export the unsed import in the `export {` object in alphabetical order\n"
+              .. "  - Only include the code snippet, no additional context or explanation is needed."
+            local agent = gp.get_command_agent()
+            gp.logger.info("Updating React SVG: " .. agent.name)
+            gp.Prompt(params, gp.Target.rewrite, agent, template, nil)
+          end,
+        },
       }
       require("gp").setup(conf)
+
+      require("which-key").add({
+        -- VISUAL mode mappings
+        -- s, x, v modes are handled the same way by which_key
+        {
+          mode = { "v" },
+          nowait = true,
+          remap = false,
+          { "<C-g><C-t>", ":<C-u>'<,'>GpChatNew tabnew<cr>", desc = "ChatNew tabnew" },
+          { "<C-g><C-v>", ":<C-u>'<,'>GpChatNew vsplit<cr>", desc = "ChatNew vsplit" },
+          { "<C-g><C-x>", ":<C-u>'<,'>GpChatNew split<cr>", desc = "ChatNew split" },
+          { "<C-g>a", ":<C-u>'<,'>GpAppend<cr>", desc = "Visual Append (after)" },
+          { "<C-g>b", ":<C-u>'<,'>GpPrepend<cr>", desc = "Visual Prepend (before)" },
+          { "<C-g>c", ":<C-u>'<,'>GpChatNew<cr>", desc = "Visual Chat New" },
+          { "<C-g>g", group = "generate into new .." },
+          { "<C-g>ge", ":<C-u>'<,'>GpEnew<cr>", desc = "Visual GpEnew" },
+          { "<C-g>gn", ":<C-u>'<,'>GpNew<cr>", desc = "Visual GpNew" },
+          { "<C-g>gp", ":<C-u>'<,'>GpPopup<cr>", desc = "Visual Popup" },
+          { "<C-g>gt", ":<C-u>'<,'>GpTabnew<cr>", desc = "Visual GpTabnew" },
+          { "<C-g>gv", ":<C-u>'<,'>GpVnew<cr>", desc = "Visual GpVnew" },
+          { "<C-g>i", ":<C-u>'<,'>GpImplement<cr>", desc = "Implement selection" },
+          { "<C-g>n", "<cmd>GpNextAgent<cr>", desc = "Next Agent" },
+          { "<C-g>p", ":<C-u>'<,'>GpChatPaste<cr>", desc = "Visual Chat Paste" },
+          { "<C-g>r", ":<C-u>'<,'>GpRewrite<cr>", desc = "Visual Rewrite" },
+          { "<C-g>s", "<cmd>GpStop<cr>", desc = "GpStop" },
+          { "<C-g>t", ":<C-u>'<,'>GpChatToggle<cr>", desc = "Visual Toggle Chat" },
+          { "<C-g>w", group = "Whisper" },
+          { "<C-g>wa", ":<C-u>'<,'>GpWhisperAppend<cr>", desc = "Whisper Append" },
+          { "<C-g>wb", ":<C-u>'<,'>GpWhisperPrepend<cr>", desc = "Whisper Prepend" },
+          { "<C-g>we", ":<C-u>'<,'>GpWhisperEnew<cr>", desc = "Whisper Enew" },
+          { "<C-g>wn", ":<C-u>'<,'>GpWhisperNew<cr>", desc = "Whisper New" },
+          { "<C-g>wp", ":<C-u>'<,'>GpWhisperPopup<cr>", desc = "Whisper Popup" },
+          { "<C-g>wr", ":<C-u>'<,'>GpWhisperRewrite<cr>", desc = "Whisper Rewrite" },
+          { "<C-g>wt", ":<C-u>'<,'>GpWhisperTabnew<cr>", desc = "Whisper Tabnew" },
+          { "<C-g>wv", ":<C-u>'<,'>GpWhisperVnew<cr>", desc = "Whisper Vnew" },
+          { "<C-g>ww", ":<C-u>'<,'>GpWhisper<cr>", desc = "Whisper" },
+          { "<C-g>x", ":<C-u>'<,'>GpContext<cr>", desc = "Visual GpContext" },
+        },
+
+        -- NORMAL mode mappings
+        {
+          mode = { "n" },
+          nowait = true,
+          remap = false,
+          { "<C-g><C-t>", "<cmd>GpChatNew tabnew<cr>", desc = "New Chat tabnew" },
+          { "<C-g><C-v>", "<cmd>GpChatNew vsplit<cr>", desc = "New Chat vsplit" },
+          { "<C-g><C-x>", "<cmd>GpChatNew split<cr>", desc = "New Chat split" },
+          { "<C-g>a", "<cmd>GpAppend<cr>", desc = "Append (after)" },
+          { "<C-g>b", "<cmd>GpPrepend<cr>", desc = "Prepend (before)" },
+          { "<C-g>c", "<cmd>GpChatNew<cr>", desc = "New Chat" },
+          { "<C-g>f", "<cmd>GpChatFinder<cr>", desc = "Chat Finder" },
+          { "<C-g>g", group = "generate into new .." },
+          { "<C-g>ge", "<cmd>GpEnew<cr>", desc = "GpEnew" },
+          { "<C-g>gn", "<cmd>GpNew<cr>", desc = "GpNew" },
+          { "<C-g>gp", "<cmd>GpPopup<cr>", desc = "Popup" },
+          { "<C-g>gt", "<cmd>GpTabnew<cr>", desc = "GpTabnew" },
+          { "<C-g>gv", "<cmd>GpVnew<cr>", desc = "GpVnew" },
+          { "<C-g>n", "<cmd>GpNextAgent<cr>", desc = "Next Agent" },
+          { "<C-g>r", "<cmd>GpRewrite<cr>", desc = "Inline Rewrite" },
+          { "<C-g>s", "<cmd>GpStop<cr>", desc = "GpStop" },
+          { "<C-g>t", "<cmd>GpChatToggle<cr>", desc = "Toggle Chat" },
+          { "<C-g>w", group = "Whisper" },
+          { "<C-g>wa", "<cmd>GpWhisperAppend<cr>", desc = "Whisper Append (after)" },
+          { "<C-g>wb", "<cmd>GpWhisperPrepend<cr>", desc = "Whisper Prepend (before)" },
+          { "<C-g>we", "<cmd>GpWhisperEnew<cr>", desc = "Whisper Enew" },
+          { "<C-g>wn", "<cmd>GpWhisperNew<cr>", desc = "Whisper New" },
+          { "<C-g>wp", "<cmd>GpWhisperPopup<cr>", desc = "Whisper Popup" },
+          { "<C-g>wr", "<cmd>GpWhisperRewrite<cr>", desc = "Whisper Inline Rewrite" },
+          { "<C-g>wt", "<cmd>GpWhisperTabnew<cr>", desc = "Whisper Tabnew" },
+          { "<C-g>wv", "<cmd>GpWhisperVnew<cr>", desc = "Whisper Vnew" },
+          { "<C-g>ww", "<cmd>GpWhisper<cr>", desc = "Whisper" },
+          { "<C-g>x", "<cmd>GpContext<cr>", desc = "Toggle GpContext" },
+        },
+
+        -- INSERT mode mappings
+        {
+          mode = { "i" },
+          nowait = true,
+          remap = false,
+          { "<C-g><C-t>", "<cmd>GpChatNew tabnew<cr>", desc = "New Chat tabnew" },
+          { "<C-g><C-v>", "<cmd>GpChatNew vsplit<cr>", desc = "New Chat vsplit" },
+          { "<C-g><C-x>", "<cmd>GpChatNew split<cr>", desc = "New Chat split" },
+          { "<C-g>a", "<cmd>GpAppend<cr>", desc = "Append (after)" },
+          { "<C-g>b", "<cmd>GpPrepend<cr>", desc = "Prepend (before)" },
+          { "<C-g>c", "<cmd>GpChatNew<cr>", desc = "New Chat" },
+          { "<C-g>f", "<cmd>GpChatFinder<cr>", desc = "Chat Finder" },
+          { "<C-g>g", group = "generate into new .." },
+          { "<C-g>ge", "<cmd>GpEnew<cr>", desc = "GpEnew" },
+          { "<C-g>gn", "<cmd>GpNew<cr>", desc = "GpNew" },
+          { "<C-g>gp", "<cmd>GpPopup<cr>", desc = "Popup" },
+          { "<C-g>gt", "<cmd>GpTabnew<cr>", desc = "GpTabnew" },
+          { "<C-g>gv", "<cmd>GpVnew<cr>", desc = "GpVnew" },
+          { "<C-g>n", "<cmd>GpNextAgent<cr>", desc = "Next Agent" },
+          { "<C-g>r", "<cmd>GpRewrite<cr>", desc = "Inline Rewrite" },
+          { "<C-g>s", "<cmd>GpStop<cr>", desc = "GpStop" },
+          { "<C-g>t", "<cmd>GpChatToggle<cr>", desc = "Toggle Chat" },
+          { "<C-g>w", group = "Whisper" },
+          { "<C-g>wa", "<cmd>GpWhisperAppend<cr>", desc = "Whisper Append (after)" },
+          { "<C-g>wb", "<cmd>GpWhisperPrepend<cr>", desc = "Whisper Prepend (before)" },
+          { "<C-g>we", "<cmd>GpWhisperEnew<cr>", desc = "Whisper Enew" },
+          { "<C-g>wn", "<cmd>GpWhisperNew<cr>", desc = "Whisper New" },
+          { "<C-g>wp", "<cmd>GpWhisperPopup<cr>", desc = "Whisper Popup" },
+          { "<C-g>wr", "<cmd>GpWhisperRewrite<cr>", desc = "Whisper Inline Rewrite" },
+          { "<C-g>wt", "<cmd>GpWhisperTabnew<cr>", desc = "Whisper Tabnew" },
+          { "<C-g>wv", "<cmd>GpWhisperVnew<cr>", desc = "Whisper Vnew" },
+          { "<C-g>ww", "<cmd>GpWhisper<cr>", desc = "Whisper" },
+          { "<C-g>x", "<cmd>GpContext<cr>", desc = "Toggle GpContext" },
+        },
+      })
     end,
   },
 }
