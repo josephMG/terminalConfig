1c19c07ac88bbcf828da809992835384a9406791
josephMG <joseph@taxigo.com.tw>
2025-07-04 18:22:39 +0800

    update nvim, add sections comment for snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -50,11 +50,16 @@ return {
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          -- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
       },
+      -- sections = {
+      --   { section = "header" },
+      --   { section = "keys", gap = 1, padding = 1 },
+      --   { title = "Sessions", padding = 1 },
+      -- },
     },
   },
   keys = {
