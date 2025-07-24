fc59e7ee796e28a66d274ceb3bc4efd8d8ecef96
JosephMG <joseph780116.mg@gmail.com>
2025-07-24 17:25:19 +0800

    update devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,5 +7,6 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 npm install -g pyright
-pip install -r requirements.txt
-poetry install
+
+uv sync --locked
+echo alias vim=nvim >> ~/.bashrc
